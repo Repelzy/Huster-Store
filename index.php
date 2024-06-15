0b038116bfc9b36817413a830a03d92826270497
Hung Manh Ngo <89698425+Repelzy@users.noreply.github.com>
2024-06-15 23:32:43 +0700

    Merge branch 'main' of github.com:Repelzy/Huster-Store

--- index.php
+++ index.php
@@ -2,16 +2,7 @@
 
 
 include "header.php";
-<<<<<<< HEAD
-=======
-
->>>>>>> c770efb3c9fe1cadd440c0a939d699cd31ecf3b9
 include "body.php";
 include "newslettter.php";
 include "footer.php";
 ?>
-<<<<<<< HEAD
-=======
-		
-		
->>>>>>> c770efb3c9fe1cadd440c0a939d699cd31ecf3b9
